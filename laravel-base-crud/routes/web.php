f9316c04b5375b6541af8cc0bab7453bcb5cf18b
Belloni-Marco-Camillo <96737807+Belloni-Marco-Camillo@users.noreply.github.com>
2022-07-07 09:34:36 +0200

    final_commit

--- laravel-base-crud/routes/web.php
+++ laravel-base-crud/routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/comics', 'ComicController@index')->name('comics.index');
+Route::get('/comics/create', 'ComicController@create')->name('comics.create');
+Route::post('/comics', 'ComicController@store')->name('comics.store');
+Route::get('/comics/{comic}', 'ComicController@show')->name('comics.show');
+Route::get('/comics/{comic}/edit', 'ComicController@edit')->name('comics.edit');
+Route::put('/comics/{comic}', 'ComicController@update')->name('comics.update');
+Route::delete('/comics/{comic}', 'ComicController@destroy')->name('comics.destroy');
